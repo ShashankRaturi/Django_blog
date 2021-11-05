a56af3d967c6284ffbb6232a8dd680f565b0110c
shashank <shashankraturi4@gmail.com>
2021-11-05 19:01:20 +0530

    updated settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -20,13 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-7ff8kb1ri@e-!pu+!-##siod%ebms)o8=!d_6cn*$0*vebwno%'
-SECRET_KEY = os.environ.get('SECRET_KEY')
 
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['django-fba.herokuapp.com']
 
@@ -153,6 +147,13 @@ LOGIN_URL = 'login'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-7ff8kb1ri@e-!pu+!-##siod%ebms)o8=!d_6cn*$0*vebwno%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
