65829d89e63a6c7e8b125990a47e5939a58cb2b3
shashank <shashankraturi4@gmail.com>
2021-11-05 14:04:28 +0530

    settings updated

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7ff8kb1ri@e-!pu+!-##siod%ebms)o8=!d_6cn*$0*vebwno%'
+# SECRET_KEY = 'django-insecure-7ff8kb1ri@e-!pu+!-##siod%ebms)o8=!d_6cn*$0*vebwno%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-fba.herokuapp.com']
 
 
 # Application definition
